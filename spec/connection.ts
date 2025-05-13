37cb0d466277ba2123e4409a5c126ba91bd2e6bd
seveibar <seveibar@gmail.com>
2025-05-13 15:35:06 -0700

    add optional connection property

--- spec/connection.ts
+++ spec/connection.ts
@@ -1,17 +1,20 @@
 import { z } from "zod"
 import { expectTypesMatch } from "../lib/utils/expectTypesMatch"
 import { PointToConnect, point_to_connect } from "./point_to_connect"
+import { type TraceId, trace_id } from "./trace_id"
 
 export interface SimpleRouteConnection {
   name: string
   netConnectionName?: string
   pointsToConnect: Array<PointToConnect>
+  source_trace_id?: TraceId
 }
 
 export const simple_route_connection = z.object({
   name: z.string(),
   netConnectionName: z.string().optional(),
   pointsToConnect: z.array(point_to_connect),
+  source_trace_id: trace_id.optional(),
 })
 
 expectTypesMatch<
